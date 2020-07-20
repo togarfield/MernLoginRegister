25b3d067408daaa0f65a1232e1ed6dd9075ec567
Abduh <mabduhsiregar@gmail.com>
2020-07-20 13:56:30 +0700

    first commit

---
+++ routes/Users.js
@@ -0,0 +1,108 @@
+const express = require("express")
+const users = express.Router()
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+const User = require("../models/User")
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+    const today = new Date()
+    const userData = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        created: today
+    }
+
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user => {
+        if (!user) {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                userData.password = hash
+                User.create(userData)
+                .then(user => {
+                    res.json({status: user.email + ' registered!'})
+                })
+                .catch(err => {
+                    res.send('error: ' + err)
+                })
+            })
+        } else {
+            res.json({error: 'User already exists'})
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+})
+
+users.post('/login', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user => {
+        if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                const payload = {
+                    _id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    email: user.email
+                }
+                let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                    expiresIn: 1440
+                })
+                // res.send(token)
+                res.status(200).send ({
+                    message: "Successful login",
+                    status: "OK",
+                    token: token
+                })
+            } else {
+                // res.json({error: "User does not exist"})
+                res.status(200).send ({
+                    message: "Email or Password is wrong",
+                    status: "ERROR"
+                })
+            }
+        } else {
+            console.log('masuk else')
+            res.status(200).send ({
+                message: "Email does not exist",
+                status: "ERROR"
+            })
+            res.json({error: "User or Password is wrong"})
+        }
+    })
+    .catch(err => {
+        console.log('masuk error catch')
+        res.send('error: ' + err)
+    })
+})
+
+users.get('/profile', (req, res) => {
+    var decoded = jwt.verify(req.header['authorization'], process.env.SECRET_KEY)
+
+    User.findOne({
+        _id: decoded._id
+    })
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.send("User does not exist")
+            }
+        })
+        .catch(err => {
+            res.send('error : ' + err)
+        })
+})
+
+module.exports = users
